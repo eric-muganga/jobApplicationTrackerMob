0f4f886895f6fe23bc0b9c2c9d1036d01a645c60
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-01-23 22:08:25 +0100

    Refactor(index.js): Wrap application with Redux Provider and GestureHandlerRootView for state management and gesture handling.

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
  */
 
 import {AppRegistry} from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
 import {name as appName} from './app.json';
+import store from './store/store';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+const RootApp = () => (
+  <Provider store={store}>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <App/>
+    </GestureHandlerRootView>
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RootApp);
