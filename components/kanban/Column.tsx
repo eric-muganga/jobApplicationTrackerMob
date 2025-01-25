88814027cd62a9ca5a385c7774bacaf0451f440c
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-01-25 22:55:25 +0100

    Refactor(Column.tsx): Integrate KanbanCard component with statusId and application details for enhanced functionality.

---
+++ components/kanban/Column.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import KanbanCard from './KanbanCard';
+
+interface ColumnProps {
+  name: string;
+  count: number;
+  applications: Array<{
+    statusId: string;
+    id: string;
+    company: string;
+    jobTitle: string;
+    status: string;
+  }>;
+}
+
+const Column: React.FC<ColumnProps> = ({ name, count, applications }) => {
+
+  console.log("Column component" ,count, name, applications);
+  return (
+    <View style={styles.column}>
+      <Text style={styles.columnHeader}>{name}</Text>
+      <Text style={styles.columnCount}>
+        {count} {count === 1 ? 'Job' : 'Jobs'}
+      </Text>
+      <FlatList
+        data={applications}
+        renderItem={({ item }) => (
+          <KanbanCard
+            id={item.id}
+            company={item.company}
+            position={item.jobTitle}
+            stage={item.status}
+            statusId={item.statusId}
+            application={item}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  column: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 8,
+    width: 250,
+    marginRight: 10,
+    minHeight: 400,
+  },
+  columnHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  columnCount: {
+    textAlign: 'center',
+    color: '#666',
+    fontSize: 12,
+    marginBottom: 10,
+  },
+});
+
+export default Column;
